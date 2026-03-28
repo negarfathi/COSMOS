9e4e5d61f17dd29fd52c181377db6b9a256a1905
anonymous <anonymous@example.com>
2026-03-28 16:41:42 -0500

    initial commit

---
+++ GetRawPathConstraints/GetRawPathConstraints/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GetRawPathConstraints
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public string ReplaceFirstChar(string target, char c)
+        {
+            if (target == null) throw new ArgumentNullException();
+            if (target.Length == 0) throw new ArgumentOutOfRangeException();
+            return c + target.Substring(1);
+        }
+    }
+}
